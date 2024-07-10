3d8050408a48aa5b77245a56487d2d9572110aa8
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-07-09 21:34:55 -0500

    Working ish

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use dotenv::dotenv;
-use serenity::prelude::*;
+use serenity::{
+    all::{ChannelId, GuildId},
+    prelude::*,
+};
 use std::env;
 use tokio::time::{sleep, Duration};
 
@@ -10,10 +13,13 @@ async fn main() {
     // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     let bot_time_str = env::var("BOT_TIME").expect("Expected a bot time");
-    let bot_time = bot_time_str.parse::<u64>().expect("Expected a number for bot time");
+    let bot_time = bot_time_str
+        .parse::<u64>()
+        .expect("Expected a number for bot time");
     // Set gateway intents, which decides what events the bot will be notified about
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::GUILDS
         | GatewayIntents::MESSAGE_CONTENT;
 
     // Create a new instance of the Client, logging in as a bot.
@@ -21,13 +27,27 @@ async fn main() {
         .await
         .expect("Err creating client");
 
-
     println!("starting a timer with interval of {} seconds", bot_time);
+
+    let http = client.http.clone();
+    let cache = client.cache.clone();
     tokio::spawn(async move {
         loop {
             //Pick someone at random here
-            println!("ayo looking");
             sleep(Duration::from_secs(bot_time)).await;
+
+            match http.get_guild(GuildId::new(1221093431364026438)).await {
+                Ok(guild) => {
+                    if let Ok(channels) = guild.channels(&http).await {
+                        if let Some(channel) = channels.get(&ChannelId::new(1221093432274194457)) {
+                            println!("channel: {:?}", channel.members(&cache));
+                        }
+                    }
+                }
+                Err(why) => {
+                    println!("Error getting guild: {:?}", why);
+                }
+            }
         }
     });
 
